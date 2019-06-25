52b46e6c3e42c4770cecede2ae092effe188c65f
Blake <blake.peterson.441@gmail.com>
2019-06-25 10:32:03 -0500

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# Intro to node
+
+node - another way to run javascript
+node package manager (npm) - download and use jQuery and other things from server and keeps them updated
+JSON - JavaScript Object Notation
+    - quotes around everything(besides empty () [] {} ) in .json
+
+
+create modules in node and can put them together
---
+++ index.js
@@ -0,0 +1,26 @@
+const character = [
+    {
+        name: "Spike",
+        show: "Cowboy Bebop"
+    },
+    {
+        name: "L",
+        show: "Death Note"
+    }
+    {
+        name: "Goku",
+        show: "Dragonball Z"
+    } {
+        name: "Gon",
+        show: "Hunter X Hunter"
+    }
+];
+
+// For loop to go through the array and write to the console 
+// something like "Spike is from Cowboy Bebop".
+
+function namesShow () {
+    for ( let i = 0; i < character.length; i++ ){
+        
+    }
+}
